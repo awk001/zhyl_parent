46242195cff267e6cce8931035692a20a3921185
awk <1574126936@qq.com>
2021-09-11 09:21:53 +0800

    Initial commit

---
+++ src/main/java/cn/code/awk/zhyl_parent/ZhylParentApplication.java
@@ -0,0 +1,13 @@
+package cn.code.awk.zhyl_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhylParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhylParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/code/awk/zhyl_parent/ZhylParentApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.code.awk.zhyl_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhylParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
