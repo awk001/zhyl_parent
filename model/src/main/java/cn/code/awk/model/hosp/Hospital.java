f926feb15caf015e5bc6a96f2fc85f9c309f8ef9
awk <1574126936@qq.com>
2021-09-12 18:49:03 +0800

    格式化代码

--- model/src/main/java/cn/code/awk/model/hosp/Hospital.java
+++ model/src/main/java/cn/code/awk/model/hosp/Hospital.java
@@ -19,50 +19,50 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @ApiModel(description = "Hospital")
 @Document("Hospital")
 public class Hospital extends BaseMongoEntity {
-	
-	private static final long serialVersionUID = 1L;
-	
-	@ApiModelProperty(value = "医院编号")
-	@Indexed(unique = true) //唯一索引
-	private String hoscode;
 
-	@ApiModelProperty(value = "医院名称")
-	@Indexed //普通索引
-	private String hosname;
+    private static final long serialVersionUID = 1L;
 
-	@ApiModelProperty(value = "医院类型")
-	private String hostype;
+    @ApiModelProperty(value = "医院编号", example = "1001")
+    @Indexed(unique = true) //唯一索引
+    private String hoscode;
 
-	@ApiModelProperty(value = "省code")
-	private String provinceCode;
+    @ApiModelProperty(value = "医院名称", example = "XXX医院")
+    @Indexed //普通索引
+    private String hosname;
 
-	@ApiModelProperty(value = "市code")
-	private String cityCode;
+    @ApiModelProperty(value = "医院类型", example = "XXX儿童医院")
+    private String hostype;
 
-	@ApiModelProperty(value = "区code")
-	private String districtCode;
+    @ApiModelProperty(value = "省code", example = "陕西省")
+    private String provinceCode;
 
-	@ApiModelProperty(value = "详情地址")
-	private String address;
+    @ApiModelProperty(value = "市code", example = "001")
+    private String cityCode;
 
-	@ApiModelProperty(value = "医院logo")
-	private String logoData;
+    @ApiModelProperty(value = "区code", example = "002")
+    private String districtCode;
 
-	@ApiModelProperty(value = "医院简介")
-	private String intro;
+    @ApiModelProperty(value = "详情地址", example = "XXX地址")
+    private String address;
 
-	@ApiModelProperty(value = "坐车路线")
-	private String route;
+    @ApiModelProperty(value = "医院logo", example = "logo图URL")
+    private String logoData;
 
-	@ApiModelProperty(value = "状态 0：未上线 1：已上线")
-	private Integer status;
+    @ApiModelProperty(value = "医院简介", example = "introduction")
+    private String intro;
 
-	//预约规则
-	@ApiModelProperty(value = "预约规则")
-	private BookingRule bookingRule;
+    @ApiModelProperty(value = "坐车路线",example = "线路")
+    private String route;
 
-	public void setBookingRule(String bookingRule) {
-		this.bookingRule = JSONObject.parseObject(bookingRule, BookingRule.class);
-	}
+    @ApiModelProperty(value = "状态 0：未上线 1：已上线", example = "0")
+    private Integer status;
+
+    //预约规则
+    @ApiModelProperty(value = "预约规则", example = "预约规则")
+    private BookingRule bookingRule;
+
+    public void setBookingRule(String bookingRule) {
+        this.bookingRule = JSONObject.parseObject(bookingRule, BookingRule.class);
+    }
 
 }
